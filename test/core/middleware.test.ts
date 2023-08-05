1a389901eee98b13a9bc3bd0d5a10d1058109ada
David Biró <dbiro97@gmail.com>
2023-08-06 00:09:24 +0200

    Extend middleware tests

--- test/core/middleware.test.ts
+++ test/core/middleware.test.ts
@@ -24,6 +24,7 @@ describe("middleware", () => {
       assert.equal(response.foo, "hi");
     });
   });
+
   it("should not return any middleware data", async () => {
     const cs = await createClientAndServer((builder) => ({
       get: builder
@@ -42,6 +43,7 @@ describe("middleware", () => {
       assert.equal((response as any).foo, undefined);
     });
   });
+
   it("should return middleware response only if next: false", async () => {
     const cs = await createClientAndServer((builder) => ({
       get: builder
@@ -50,6 +52,38 @@ describe("middleware", () => {
             next: false,
             statusCode: 400 as const,
             foo: "hello",
+            bar: 42,
+          };
+        })
+        .get(async () => {
+          assert.ok(false, "This should not be called");
+          return success({});
+        }),
+    }));
+    await cs.run(async (client) => {
+      const response = await client.get.get({});
+      if (response.statusCode !== 400)
+        assert.ok(false, "statusCode should be 400");
+      assert.equal(response.foo, "hello");
+      assert.equal(response.bar, "42");
+    });
+  });
+
+  it("should keep the order of middlewares 1", async () => {
+    const cs = await createClientAndServer((builder) => ({
+      get: builder
+        .middleware(async () => {
+          return {
+            next: false,
+            statusCode: 400 as const,
+            mw: 1,
+          };
+        })
+        .middleware(async () => {
+          return {
+            next: false,
+            statusCode: 400 as const,
+            mw: 2,
           };
         })
         .get(async () => {
@@ -58,8 +92,97 @@ describe("middleware", () => {
     }));
     await cs.run(async (client) => {
       const response = await client.get.get({});
-      assert.equal(response.statusCode, 400);
-      if (response.statusCode === 400) assert.equal(response.foo, "hello");
+      if (response.statusCode !== 400)
+        assert.ok(false, "statusCode should be 400");
+      assert.equal(response.mw, 1);
     });
+  });
+
+  it("should keep the order of middlewares 2", async () => {
+    const cs = await createClientAndServer((builder) => ({
+      get: builder
+        .middleware(async () => {
+          return {
+            next: true,
+          };
+        })
+        .middleware(async () => {
+          return {
+            next: false,
+            statusCode: 400 as const,
+            mw: 2,
+          };
+        })
+        .get(async () => {
+          return success({});
+        }),
+    }));
+    await cs.run(async (client) => {
+      const response = await client.get.get({});
+      if (response.statusCode !== 400)
+        assert.ok(false, "statusCode should be 400");
+      assert.equal(response.mw, 2);
+    });
+  });
+
+  it("should pass data from top to bottom", async () => {
+    let tested = false;
+    const cs = await createClientAndServer((builder) => ({
+      get: builder
+        .middleware(async () => {
+          return {
+            next: true,
+            foo: 1,
+          };
+        })
+        .middleware(async ({ data }) => {
+          assert.equal(data.foo, 1);
+          return {
+            next: true,
+            bar: 2,
+          };
+        })
+        .get(async ({ data }) => {
+          assert.equal(data.foo, 1);
+          assert.equal(data.bar, 2);
+          tested = true;
+          return success({});
+        }),
+    }));
+    await cs.run(async (client) => {
+      const response = await client.get.get({});
+      assert.equal(response.result, "success");
+    });
+    assert.equal(tested, true);
+  });
+
+  it("should overwrite previous middleware data if the name is matching", async () => {
+    let tested = false;
+    const cs = await createClientAndServer((builder) => ({
+      get: builder
+        .middleware(async () => {
+          return {
+            next: true,
+            foo: 1,
+          };
+        })
+        .middleware(async ({ data }) => {
+          assert.equal(data.foo, 1);
+          return {
+            next: true,
+            foo: 2,
+          };
+        })
+        .get(async ({ data }) => {
+          assert.equal(data.foo, 2);
+          tested = true;
+          return success({});
+        }),
+    }));
+    await cs.run(async (client) => {
+      const response = await client.get.get({});
+      assert.equal(response.result, "success");
+    });
+    assert.equal(tested, true);
   });
 });
