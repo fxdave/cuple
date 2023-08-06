cea2661bedf5e9d490240eebb4240b3adab74c3a
David Biró <dbiro97@gmail.com>
2023-08-06 16:11:57 +0200

    Add examples back

---
+++ examples/blog/example-client.ts
@@ -0,0 +1,47 @@
+import { createClient } from "../../src/client";
+import type { routes } from "./example";
+
+const client = createClient<typeof routes>({
+  path: "http://localhost:8080/rpc",
+});
+
+async function test() {
+  console.log("getPosts(): ", await getPosts());
+  console.log("getPost(1): ", await getPost(1));
+  console.log("deletePost(1): ", await deletePost(1));
+  console.log("getPost(1): ", await getPost(1));
+  const newPost = await addPost("Some Thing", "There's something.");
+  console.log("newPost: ", newPost);
+  console.log("getPost(newPost.post.id): ", await getPost(newPost.post.id));
+}
+
+async function getPosts() {
+  return await client.getPosts.get({});
+}
+
+async function getPost(id: number) {
+  return await client.getPost.get({
+    params: {
+      id,
+    },
+  });
+}
+
+async function deletePost(id: number) {
+  return await client.deletePost.delete({
+    params: {
+      id,
+    },
+  });
+}
+
+async function addPost(title: string, content: string) {
+  return await client.addPost.post({
+    body: {
+      content,
+      title,
+    },
+  });
+}
+
+test();
---
+++ examples/blog/example.ts
@@ -0,0 +1,84 @@
+import express from "express";
+import { z } from "zod";
+import { createBuilder, initRpc } from "../../src/server";
+import { apiResponse, success } from "../../src/server/responses";
+
+const app = express();
+app.use(express.json());
+const builder = createBuilder(app);
+
+const posts = [
+  { id: 1, title: "Hi again", content: "This is my second post" },
+  { id: 2, title: "Hi", content: "This is my first post" },
+];
+
+export const routes = {
+  getPosts: builder.get(async () => {
+    return success({ posts });
+  }),
+  getPost: builder
+    .paramsSchema(
+      z.object({
+        id: z.coerce.number(),
+      })
+    )
+    .get(async ({ data }) => {
+      const post = posts.find((p) => p.id === data.params.id);
+      if (!post)
+        return apiResponse("notFound", 404, {
+          message: "This post is not found",
+        });
+
+      return success({
+        post,
+      });
+    }),
+  addPost: builder
+    .bodySchema(
+      z.object({
+        title: z.string(),
+        content: z.string(),
+      })
+    )
+    .post(async ({ data }) => {
+      const nextId =
+        Math.max.apply(
+          Math,
+          posts.map((post) => post.id)
+        ) + 1;
+      const newPost = { id: nextId, ...data.body };
+      posts.push(newPost);
+      return success({
+        message: "The post has been created successfully",
+        post: newPost,
+      });
+    }),
+  deletePost: builder
+    .paramsSchema(
+      z.object({
+        id: z.coerce.number(),
+      })
+    )
+    .delete(async ({ data }) => {
+      const idx = posts.findIndex((post) => post.id === data.params.id);
+      if (idx === -1)
+        return apiResponse("notFound", 404, {
+          message: "This post is not found",
+        });
+
+      posts.splice(idx, 1);
+
+      return success({
+        message: "This post has been deleted successfully",
+      });
+    }),
+};
+
+initRpc(app, {
+  path: "/rpc",
+  routes,
+});
+
+app.listen(8080, () => {
+  console.log(`Example app listening on port 8080`);
+});
